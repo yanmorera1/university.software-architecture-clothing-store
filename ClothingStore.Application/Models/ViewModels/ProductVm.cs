0a7fa725a7fb4eebc563661764712cd6c08b1276
Yan Carlos Morera Caldas <yanmorera1@gmail.com>
2022-09-26 19:44:56 -0500

    Update: Available and Id properties added to ProductVm

--- ClothingStore.Application/Models/ViewModels/ProductVm.cs
+++ ClothingStore.Application/Models/ViewModels/ProductVm.cs
@@ -2,10 +2,12 @@
 {
     public class ProductVm
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Color { get; set; }
         public int SizeId { get; set; }
+        public bool Available { get; set; }
         public int TypeId { get; set; }
     }
 }
