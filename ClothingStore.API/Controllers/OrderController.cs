fe11c3067df137e24d40dd0ae7ff3d887fb1ba41
Yan Carlos Morera Caldas <yanmorera1@gmail.com>
2022-09-26 19:45:11 -0500

    Add: OrderController

---
+++ ClothingStore.API/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using ClothingStore.Application.Features.Orders.Commands.CreateOrder;
+using ClothingStore.Application.Features.Orders.Queries.GetOrderById;
+using ClothingStore.Application.Models.ViewModels;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClothingStore.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderVm>>> GetOrderById([FromQuery] GetOrderByIdQuery query)
+        {
+            return Ok(await _mediator.Send(query));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> CreateOrder([FromBody] CreateOrderCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+    }
+}
