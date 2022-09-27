a83a1fca42f360cef7bf7fabce988b6d06d90383
Yan Carlos Morera Caldas <yanmorera1@gmail.com>
2022-09-26 19:43:31 -0500

    Add: EmailService interface and implementation but not functional

---
+++ ClothingStore.Application/Contracts/Services/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace ClothingStore.Application.Contracts.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string to, string message);
+    }
+}
---
+++ ClothingStore.Infrastructure/Services/Email/EmailService.cs
@@ -0,0 +1,12 @@
+﻿using ClothingStore.Application.Contracts.Services;
+
+namespace ClothingStore.Infrastructure.Services.Email
+{
+    public class EmailService : IEmailService
+    {
+        public Task SendEmailAsync(string to, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
