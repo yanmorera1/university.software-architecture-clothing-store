c9354fc46873277c393ff13b3dae39fe97aa27c8
Yan Carlos Morera Caldas <yanmorera1@gmail.com>
2022-09-26 19:44:11 -0500

    Update: Available property added

--- ClothingStore.Domain/Product.cs
+++ ClothingStore.Domain/Product.cs
@@ -8,6 +8,7 @@ namespace ClothingStore.Domain
         public decimal Price { get; set; }
         public string Color { get; set; }
         public int SizeId { get; set; }
+        public bool Available { get; set; }
         public virtual Size Size { get; set; }
         public int TypeId { get; set; }
         public virtual Type Type { get; set; }
